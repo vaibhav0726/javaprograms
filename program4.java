b7014ef201c36c4f7e7d4ea57400afba4757ed1c
priyanshu awasthi <priyanshu.awas011@gmail.com>
2021-11-28 21:52:51 +0530

    changes

--- program4.java
+++ program4.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 class AreaOfCircle 
 {
    public static void main(String args[]) 
-    {   
-       
-      Scanner s= new Scanner(System.in);
+    {     
+      Scanner e = new Scanner(System.in);
         
          System.out.println("Enter the radius:");
-         double r= s.nextDouble();
+         double r= e.nextDouble();
          double  area=(22*r*r)/7 ;
          System.out.println("Area of Circle is: " + area);      
    }
