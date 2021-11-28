2a01f1d5f46297633d068832e3c54624c4f7297b
priyanshu awasthi <priyanshu.awas011@gmail.com>
2021-11-28 21:42:32 +0530

    program8

---
+++ program8.java
@@ -0,0 +1,25 @@
+class stringexample2
+{
+	public static void main(String args[])
+	{
+		String str1="JaVa Programming";
+		String str2="JaVa programming";
+		System.out.println(str1);
+		
+		System.out.println("\n\n----------\n\n");
+		
+		//char res=str1.charAt(5);
+		
+		//int res=str1.indexOf('P',6);//if after 6 P is not present simply it will print -1;
+
+		String res=str1.substring(6);
+ 
+		//boolean res=str1.equals(str2);//if we want to remove case sensivity then we will add equalsIgnoreCase();
+
+		//boolean res=str1.equalsIgnoreCase(str2);
+
+		//String res=str1.concat(str2);
+		System.out.println(res);
+		
+	}
+}
